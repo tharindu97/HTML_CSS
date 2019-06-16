f0876cf4503f34b43b86c18dd9a5d864955b5c56
Tharindu Kavishna <39029828+tharindu97@users.noreply.github.com>
2019-06-16 09:00:34 +0530

    Animations

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Animations</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="welcome">
+		<h1>Tharindu Kavishna</h1>
+	</div>
+	<div class="square">
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+
+	<div class="circle">
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+</body>
+</html>
